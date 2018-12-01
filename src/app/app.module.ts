31dc48ff19a7a7265729acb310e9e43d1c52463a
Karl-Erik Borkmann <karlerikb@users.noreply.github.com>
2018-12-01 17:34:12 +0200

    added components: create-todo, todo-list, todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTodoComponent,
+    TodoListComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule
