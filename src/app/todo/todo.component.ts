752635a6925664540d540f77da30a66549c8339a
Elle Elisa Ivantšikova <elleelisa@users.noreply.github.com>
2018-12-02 12:52:42 +0200

    Working delete button

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TodoServerService } from '../todo-server.service';
 
 @Component({
   selector: 'app-todo',
@@ -9,9 +10,12 @@ export class TodoComponent implements OnInit {
 
   @Input() todoObj;
 
-  constructor() { }
+  constructor(private todoServer: TodoServerService) { }
 
   ngOnInit() {
   }
 
+  removeTodo() {
+    this.todoServer.deleteTodo(this.todoObj);
+  }
 }
